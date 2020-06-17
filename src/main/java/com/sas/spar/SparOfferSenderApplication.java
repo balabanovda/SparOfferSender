a5254d73e2cb987e43667e0818171c6b15a07c61
userci3 <balabanovda@gmail.com>
2020-06-17 12:41:56 +0300

    Рабочая версия

--- src/main/java/com/sas/spar/SparOfferSenderApplication.java
+++ src/main/java/com/sas/spar/SparOfferSenderApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SparOfferSenderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SparOfferSenderApplication.class, args);
 	}
+
 
 }
